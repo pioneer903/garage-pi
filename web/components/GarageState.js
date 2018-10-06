471e298edbd42819e0db2d42eddcd2ae1e7d1248
pioneer903 <pioneer903@gmail.com>
2018-10-05 21:44:52 -0400

    Added second garage state
    Signed-off-by: pioneer903 <pioneer903@gmail.com>

--- web/components/GarageState.js
+++ web/components/GarageState.js
@@ -11,4 +11,17 @@ const GarageState = (props) => {
   )
 };
 
+
+const GarageState2 = (props) => {
+  let { getGarageStatus2 } = props;
+
+  return (
+    <div className="state">
+      <span>The garage door 2 is</span>
+      <p>{getGarageStatus2()}</p>
+    </div>
+  )
+};
+
 export default GarageState
+export default GarageState2
